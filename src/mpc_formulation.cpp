bc42a756f1d7525952f9d9553f6369ebe469969e
kohonda <0905honda@gmail.com>
2021-08-27 16:50:23 +0900

    fix formulation bugs

--- src/mpc_formulation.cpp
+++ src/mpc_formulation.cpp
@@ -16,8 +16,8 @@ namespace cgmres
     void NMPCModel::phixFunc([[maybe_unused]] const double t, const std::vector<double> &x, [[maybe_unused]] std::function<double(double)> &traj_curvature, [[maybe_unused]] std::function<double(double)> &traj_speed,
                              [[maybe_unused]] std::function<double(double)> &drivable_width, std::vector<double> &phix) const
     {
-        // const double curvature = traj_curvature(x[2]);
-        // const double ref_speed = traj_speed(x[2]);
+        // const double curvature = traj_curvature(x[MPC_STATE_SPACE::X_F]);
+        // const double ref_speed = traj_speed(x[MPC_STATE_SPACE::X_F]);
 
         phix[MPC_STATE_SPACE::X_F] = (1.0 / 2.0) * q_terminal_[MPC_STATE_SPACE::X_F] * (2 * x[MPC_STATE_SPACE::X_F] - 2 * x_ref_[MPC_STATE_SPACE::X_F]);
         phix[MPC_STATE_SPACE::Y_F] = (1.0 / 2.0) * q_terminal_[MPC_STATE_SPACE::Y_F] * (2 * x[MPC_STATE_SPACE::Y_F] - 2 * x_ref_[MPC_STATE_SPACE::Y_F]);
@@ -27,7 +27,7 @@ namespace cgmres
     void NMPCModel::hxFunc([[maybe_unused]] const double t, const std::vector<double> &x, const double *u, const std::vector<double> &lmd, std::function<double(double)> &traj_curvature,
                            [[maybe_unused]] std::function<double(double)> &traj_speed, [[maybe_unused]] std::function<double(double)> &drivable_width, std::vector<double> &hx) const
     {
-        const double curvature = traj_curvature(x[2]);
+        const double curvature = traj_curvature(x[MPC_STATE_SPACE::X_F]);
         // const double ref_speed = traj_speed(x[2]);
 
         hx[MPC_STATE_SPACE::X_F] = (1.0 / 2.0) * q_[MPC_STATE_SPACE::X_F] * (2 * x[MPC_STATE_SPACE::X_F] - 2 * x_ref_[MPC_STATE_SPACE::X_F]);
@@ -38,8 +38,8 @@ namespace cgmres
     void NMPCModel::huFunc([[maybe_unused]] const double t, const std::vector<double> &x, const double *u, const std::vector<double> &lmd, std::function<double(double)> &traj_curvature,
                            std::function<double(double)> &traj_speed, [[maybe_unused]] std::function<double(double)> &drivable_width, double *hu) const
     {
-        const double curvature = traj_curvature(x[2]);
-        const double ref_speed = traj_speed(x[2]);
+        const double curvature = traj_curvature(x[MPC_STATE_SPACE::X_F]);
+        const double ref_speed = traj_speed(x[MPC_STATE_SPACE::X_F]);
 
         hu[MPC_INPUT::ANGULAR_VEL_YAW] = lmd[MPC_STATE_SPACE::YAW_F] + 1.0 * r_[MPC_INPUT::ANGULAR_VEL_YAW] * u[MPC_INPUT::ANGULAR_VEL_YAW];
         hu[MPC_INPUT::TWIST_X] = -curvature * lmd[MPC_STATE_SPACE::YAW_F] * cos(x[MPC_STATE_SPACE::YAW_F]) / (-curvature * x[MPC_STATE_SPACE::Y_F] + 1) + lmd[MPC_STATE_SPACE::X_F] * cos(x[MPC_STATE_SPACE::YAW_F]) / (-curvature * x[MPC_STATE_SPACE::Y_F] + 1) + lmd[MPC_STATE_SPACE::Y_F] * sin(x[MPC_STATE_SPACE::YAW_F]) + 0.5 * r_[MPC_INPUT::TWIST_X] * (-2 * ref_speed + 2 * u[MPC_INPUT::TWIST_X]);
